d32e8a2f7763b9d863c70a6b9d32ddc57f5c87c4
xu842251462 <xu842251462@gmail.com>
2019-11-24 16:08:07 -0500

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
 import { products } from '../products';
 import { CartService } from '../cart.service';
 
