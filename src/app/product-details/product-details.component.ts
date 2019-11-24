cabde0db57844ed3844f440f7f1e6b29c35fad1f
xu842251462 <xu842251462@gmail.com>
2019-11-24 16:04:30 -0500

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { products } from "../products";
-import { CartService } from "../cart.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: "app-product-details",
